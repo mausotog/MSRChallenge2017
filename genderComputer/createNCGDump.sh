6afada234fbe14405fe4ced64b431a24c12e4a1c
Mauricio Soto <mausotog@hotmail.com>
2017-01-20 18:05:02 -0500

    Changing name of some files

--- genderComputer/createNCGDump.sh
+++ genderComputer/createNCGDump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f genderOnlyDataSet.csv
+rm -f NCGDataSet.csv
 #datasetFile=../3ParamsHead.csv
 datasetFile=../travisTorrent3ParamsNoRepeat.csv
 while read dataRow
@@ -9,8 +9,8 @@ projectName=$(echo $dataRow | cut -d "," -f1)
 commitHash=$(echo $dataRow | cut -d "," -f2)
 commitSite=https://github.com/"$projectName"/commit/"$commitHash"
 echo $commitSite
-developerGender=$(python ExtractGender.py $commitSite)
-echo "$developerGender"
-echo "$developerGender" >> genderOnlyDataSet.csv
+developerNCG=$(python ExtractNCG.py $commitSite)
+echo "$developerNCG"
+echo "$developerNCG" >> NCGDataSet.csv
 done < $datasetFile 
 
--- genderComputer/tryingGenderComputer.py
+++
@@ -1,4 +0,0 @@
-import os
-from genderComputer import GenderComputer
-gc = GenderComputer(os.path.abspath('./nameLists'))
-print gc.resolveGender(unicode('Mauricia Soto'), unicode('Costa Rica'))
